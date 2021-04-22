1d7ab62cf217bc383eb629cbf0baae311cebc307
calvin <calvin.francis96@outlook.com>
2021-04-22 08:37:51 -0400

    Generate div containing info of random character

--- OJECTIVES.md
+++ OJECTIVES.md
@@ -4,8 +4,13 @@
 # MVP
 - Users should be able to select a character from a dropdown menu and have character information displayed on the webpage
 - Alternatively, users should be able to search for a character by name
+
 # Stretch
+-generate random character quote
+
 # Wireframing
+[]Think about and sketch basic site layout
+
 # Roadmap
 [x]Create file structure
     [x]index.html
@@ -15,8 +20,8 @@
 []Set up HTML
 
 []Data exploration
-    []Look through API and test endpoints
-    []Look at how the returned data is structured
+    [x]Look through API and test endpoints
+    [x]Look at how the returned data is structured
     []Retrieve specific data
 
 []Handle Data
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('Hello')
+const baseUrl = 'https://www.breakingbadapi.com/api/';
+
+//Fetching character info from API
+function getAllCharacters() {
+    return fetch(`${baseUrl}characters`).then(resp => resp.json())
+}
+// getAllCharacters().then(characters => console.log(characters[0].name.replace(' ', '+')))
+
+//Using character info to create dropdown menu with list of characters: NOT WORKING
+function createCharacterDropdown() {
+    const select = document.createElement('select');
+    getAllCharacters().then(characters => {
+        characters.forEach(character => {
+            const option = document.createElement('option');
+            option.value = character.name.replaceAll(' ', '+');
+            option.innerText = character.name
+            select.appendChild(option)
+        })
+        // console.log(select)//console log from .then()
+    })
+    return select
+}
+
+//Fetching random characterfrom API
+function getRandomCharacter() {
+    return fetch(`${baseUrl}character/random`).then(res => res.json())
+}
+getRandomCharacter().then(character => console.log(character[0].name))
+
+//Create character div
+function createCharacterDiv(character) {
+    const characterDiv = document.createElement('div');
+    const name = document.createElement('h3');
+    name.innerText = character[0].name;
+    const image = document.createElement('img');
+    image.src = character[0].img;
+    const alias = document.createElement('p');
+    alias.innerText = character[0].nickname;
+    const job = document.createElement('p');
+    job.innerText = character[0].occupation;
+    const actor = document.createElement('p');
+    actor.innerText = character[0].portrayed;
+    const currentStatus = document.createElement('p')
+    currentStatus.innertext = character[0].status;
+    characterDiv.append(name, image, alias, job, actor, currentStatus)
+    return characterDiv
+}
+const body = document.getElementsByTagName('body')[0]
+getRandomCharacter().then(character => console.log(character))
+getRandomCharacter().then(character => body.appendChild(createCharacterDiv(character)))
